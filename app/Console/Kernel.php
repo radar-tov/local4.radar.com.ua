83ed6cb26edf0ae012b7595d19b43280b967ee2a
Evgenii-home <7985106@mail.ru>
2016-11-28 20:20:23 +0200

    16.11.28.7

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,6 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         //$schedule->command('xmlsitemap')->dailyAt('03:00');
-        $schedule->command('xmlsitemap')->cron('* * * * *')->sendOutputTo("resources/views/frontend/sitemap/shed_log.txt");
+        $schedule->command('xmlsitemap')->cron('* * * * *')->sendOutputTo("storage/app/shed_log.txt");
     }
 }
