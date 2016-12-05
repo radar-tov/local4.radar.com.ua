034577c34e5615760cafa50bee3a5d2e49ad1d86
Evgenii <7985106@mail.ru>
2016-12-05 11:18:54 +0200

    Изменена отправка почты

--- resources/views/mail/contact.blade.php
+++ resources/views/mail/contact.blade.php
@@ -1,2 +1,18 @@
-<h1>{{ $data['name'] }}</h1>
-<div>{{ $data['text'] }}</div>
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>{{ $data['subject'] }}</title>
+</head>
+<body>
+<h1>{{ $data['subject'] }}</h1>
+<h2>Имя:</h2>
+<p>{{ $data['name'] }}</p>
+<h2>Email:</h2>
+<p>{{ $data['email'] }}</p>
+<h2>Телефон:</h2>
+<p>{{ $data['phone'] }}</p>
+<h3>Коментарий</h3>
+<text>{{ $data['comment'] }}</text>
+</body>
+</html>
