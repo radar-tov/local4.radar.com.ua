16c8516c4c882bf1f38cb5f21994db222b6ecd9d
Evgenii <7985106@mail.ru>
2016-12-12 11:01:09 +0200

    Слайдер партнёры

--- resources/views/frontend/partials/products/our_partners_slider.blade.php
+++ resources/views/frontend/partials/products/our_partners_slider.blade.php
@@ -5,7 +5,7 @@
   <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/ariston_logo.gif" alt="Slider 2" /></a></div>
   <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/atem_logo.png" alt="Slider 3" /></a></div>
   <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/baxi_logo.png" alt="Slider 4" /></a></div>
-  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/danko_logo.gif" alt="Slider 5" /></a></div>
+  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/danko_logo.png" alt="Slider 5" class="par-1"/></a></div>
   <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/Electrolu_logo.png" alt="Slider 6" /></a></div>
   <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/ferroli_logo.png" alt="Slider 7" /></a></div>
   <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/gorenje_logo.png" alt="Slider 8" /></a></div>
@@ -14,7 +14,7 @@
   <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/mayak_logo.png" alt="Slider 11" /></a></div>
   <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/Metalac_logo.png" alt="Slider 12" /></a></div>
   <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/protherm_logo.gif" alt="Slider 13" /></a></div>
-  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/termaxi_logo.jpg" alt="Slider 14" /></a></div>
+  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/termaxi_logo.gif" alt="Slider 14" /></a></div>
   <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/termobar_logo.gif" alt="Slider 15" /></a></div>
   <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/TESY_logo.png" alt="Slider 16" /></a></div>
   <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/titan_logo.gif" alt="Slider 17" /></a></div>
