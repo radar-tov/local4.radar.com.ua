cbb3a274fb8945cb36fbd42a0e7c5785fe905376
Evgenii <7985106@mail.ru>
2016-12-22 09:37:16 +0200

    16.12.22.09.37

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -96,20 +96,20 @@ class FrontendController extends BaseController
 			$date = new \DateTime($category->updated_at);
 
 			//Получаем header If-Modified-Since
-//            $ifModifiedSince = strtotime(substr($request->header('If-Modified-Since'), 5));
-//            $LastModified = strtotime(substr($date->format("D, d M Y H:i:s"), 5));
-//            if($ifModifiedSince){
-//                if($ifModifiedSince >= $LastModified){
-//                    if(env('APP_ENV') == 'production'){
-//						if($_ENV['BOT']){
-//							return Response::view('frontend.subcategories', compact('categories','category'), 304);
-//						}
-//					}
-//                }
-//            }
+            $ifModifiedSince = strtotime(substr($request->header('If-Modified-Since'), 5));
+            $LastModified = strtotime(substr($date->format("D, d M Y H:i:s"), 5));
+            if($ifModifiedSince){
+                if($ifModifiedSince >= $LastModified){
+                    if(env('APP_ENV') == 'production'){
+						if($_ENV['BOT']){
+							//return Response::view('frontend.subcategories', compact('categories','category'), 304);
+						}
+					}
+                }
+            }
 
-			return Response::view('frontend.subcategories', compact('categories','category'))
-				->header( 'Last-Modified', $date->format("D, d M Y H:i:s").' GMT');
+			return Response::view('frontend.subcategories', compact('categories','category'));
+				//->header( 'Last-Modified', $date->format("D, d M Y H:i:s").' GMT')
 	  	}
 
 		if($request->ajax()){
@@ -128,21 +128,21 @@ class FrontendController extends BaseController
 
 
 		//Получаем header If-Modified-Since
-//		$ifModifiedSince = strtotime(substr($request->header('If-Modified-Since'), 5));
-//		$LastModified = strtotime(substr($date->format("D, d M Y H:i:s"), 5));
-//		if($ifModifiedSince){
-//			if($ifModifiedSince >= $LastModified){
-//				if(env('APP_ENV') == 'production'){
-//					if($_ENV['BOT']){
-//						return Response::view('frontend.catalog', compact('subcategory', 'category'), 304);
-//					}
-//				}
-//			}
-//		}
+		$ifModifiedSince = strtotime(substr($request->header('If-Modified-Since'), 5));
+		$LastModified = strtotime(substr($date->format("D, d M Y H:i:s"), 5));
+		if($ifModifiedSince){
+			if($ifModifiedSince >= $LastModified){
+				if(env('APP_ENV') == 'production'){
+					if($_ENV['BOT']){
+						//return Response::view('frontend.catalog', compact('subcategory', 'category'), 304);
+					}
+				}
+			}
+		}
 
 
-		return Response::view('frontend.catalog', compact('subcategory', 'category'))
-			->header( 'Last-Modified', $date->format("D, d M Y H:i:s").' GMT');
+		return Response::view('frontend.catalog', compact('subcategory', 'category'));
+			//->header( 'Last-Modified', $date->format("D, d M Y H:i:s").' GMT')
 
 	}
 
