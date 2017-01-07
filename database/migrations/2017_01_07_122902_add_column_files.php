3bbe081337a34c43814844aa7bcc955de4fe1276
Evgenii-home <7985106@mail.ru>
2017-01-07 19:03:32 +0200

    add migration

---
+++ database/migrations/2017_01_07_122902_add_column_files.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnFiles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('files', function (Blueprint $table) {
+            $table->integer('brand_id')->after('category_id')->default(7);
+            $table->integer('order')->after('brand_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('files', function (Blueprint $table) {
+            $table->dropColumn('brand_id', 'order');
+        });
+    }
+}
