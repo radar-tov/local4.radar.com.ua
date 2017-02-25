e8d5f6bd9fba50a01c28239cf59de825e6a8a442
Evgenii-home <7985106@mail.ru>
2017-02-25 07:30:29 +0200

    17.02.25.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,23 @@
-/*
-    CSS
-
-    // frontend
-    public\frontend\css\print.css
-    public\frontend\css\style.css
-    public\frontend\css\font.css
-    public\css\additional.css
-
-    //авторизация при входе в админку
-    public\css\app.css
-
-    // админка
-    public\admin\assets\css\bootstrap.min.css
-    public\admin\assets\css\font-awesome.min.css
-    public\admin\assets\css\jquery-ui.custom.min.css
-    public\admin\assets\css\jquery.gritter.css
-    https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css
-    public\admin\assets\css\ace-fonts.css
-    public\admin\assets\css\uncompressed\ace.css
-    public\admin\assets\css\uncompressed\ace-part2.css  // для IE9
-    public\admin\assets\css\ace-ie.min.css              // для IE9
-    public\admin\assets\css\main.css
-    public\packages\colorbox\colorbox.css
-    public\packages\tinymce\skins\lightgray\skin.min.css
-
-
-*/
-
-
 var elixir = require('laravel-elixir');
 
 
 elixir(function(mix) {
     mix.styles(
         [
-            'style.css',
-            'additional.css',
-            'jquery.fancybox.css',
-            'font.css',
+            'frontend/style.css',
+            'frontend/additional.css',
+            'frontend/jquery.fancybox.css',
+            'frontend/font.css',
         ]
     );
 
 
     mix.scripts(
         [
-            'jquery-2.1.3.min.js',
-            'jquery.mousewheel.pack.js',
-            'jquery.fancybox.pack.js',
-            'index.js'
+            'frontend/jquery-2.1.3.min.js',
+            'frontend/jquery.mousewheel.pack.js',
+            'frontend/jquery.fancybox.pack.js',
+            'frontend/index.js'
         ]
     );
 
