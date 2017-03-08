90f409c66502bfb3c7cdac377dc547cc42c51f49
Evgenii-home <7985106@mail.ru>
2017-03-08 08:23:57 +0200

    В слайдере партнёры запрос на изображения не прямой запрос на public

--- resources/views/frontend/partials/products/our_partners_slider.blade.php
+++ resources/views/frontend/partials/products/our_partners_slider.blade.php
@@ -1,24 +1,24 @@
 <!-- resources.views.frontend.partials.products.our_partners_slider -->
 <h3>Наши партнеры</h3>
 <div class="partners">
-  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/alltermo_logo.png" alt="Slider 1"/></a></div>
-  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/ariston_logo.gif" alt="Slider 2"/></a></div>
-  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/atem_logo.png" alt="Slider 3"/></a></div>
-  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/baxi_logo.png" alt="Slider 4"/></a></div>
-  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/danko_logo.png" alt="Slider 5" class="par-1"/></a></div>
-  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/Electrolu_logo.png" alt="Slider 6"/></a></div>
-  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/ferroli_logo.png" alt="Slider 7"/></a></div>
-  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/gorenje_logo.png" alt="Slider 8"/></a></div>
-  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/HotWell_logo.gif" alt="Slider 9"/></a></div>
-  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/konvektor_logo.png" alt="Slider 10"/></a></div>
-  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/mayak_logo.png" alt="Slider 11"/></a></div>
-  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/Metalac_logo.png" alt="Slider 12"/></a></div>
-  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/protherm_logo.gif" alt="Slider 13"/></a></div>
-  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/termaxi_logo.gif" alt="Slider 14"/></a></div>
-  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/termobar_logo.gif" alt="Slider 15"/></a></div>
-  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/TESY_logo.png" alt="Slider 16"/></a></div>
-  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/titan_logo.gif" alt="Slider 17"/></a></div>
-  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/Vaillant_logo.png" alt="Slider 18"/></a></div>
-  <div><a href="/proizvoditeli"><img src="/public/frontend/images/partners/viadrus_logo.jpg" alt="Slider 19"/></a></div>
+  <div><a href="/proizvoditeli"><img src="frontend/images/partners/alltermo_logo.png" alt="Slider 1"/></a></div>
+  <div><a href="/proizvoditeli"><img src="frontend/images/partners/ariston_logo.gif" alt="Slider 2"/></a></div>
+  <div><a href="/proizvoditeli"><img src="frontend/images/partners/atem_logo.png" alt="Slider 3"/></a></div>
+  <div><a href="/proizvoditeli"><img src="frontend/images/partners/baxi_logo.png" alt="Slider 4"/></a></div>
+  <div><a href="/proizvoditeli"><img src="frontend/images/partners/danko_logo.png" alt="Slider 5" class="par-1"/></a></div>
+  <div><a href="/proizvoditeli"><img src="frontend/images/partners/Electrolu_logo.png" alt="Slider 6"/></a></div>
+  <div><a href="/proizvoditeli"><img src="frontend/images/partners/ferroli_logo.png" alt="Slider 7"/></a></div>
+  <div><a href="/proizvoditeli"><img src="frontend/images/partners/gorenje_logo.png" alt="Slider 8"/></a></div>
+  <div><a href="/proizvoditeli"><img src="frontend/images/partners/HotWell_logo.gif" alt="Slider 9"/></a></div>
+  <div><a href="/proizvoditeli"><img src="frontend/images/partners/konvektor_logo.png" alt="Slider 10"/></a></div>
+  <div><a href="/proizvoditeli"><img src="frontend/images/partners/mayak_logo.png" alt="Slider 11"/></a></div>
+  <div><a href="/proizvoditeli"><img src="frontend/images/partners/Metalac_logo.png" alt="Slider 12"/></a></div>
+  <div><a href="/proizvoditeli"><img src="frontend/images/partners/protherm_logo.gif" alt="Slider 13"/></a></div>
+  <div><a href="/proizvoditeli"><img src="frontend/images/partners/termaxi_logo.gif" alt="Slider 14"/></a></div>
+  <div><a href="/proizvoditeli"><img src="frontend/images/partners/termobar_logo.gif" alt="Slider 15"/></a></div>
+  <div><a href="/proizvoditeli"><img src="frontend/images/partners/TESY_logo.png" alt="Slider 16"/></a></div>
+  <div><a href="/proizvoditeli"><img src="frontend/images/partners/titan_logo.gif" alt="Slider 17"/></a></div>
+  <div><a href="/proizvoditeli"><img src="frontend/images/partners/Vaillant_logo.png" alt="Slider 18"/></a></div>
+  <div><a href="/proizvoditeli"><img src="frontend/images/partners/viadrus_logo.jpg" alt="Slider 19"/></a></div>
 </div>
 <!-- / resources.views.frontend.partials.products.our_partners_slider -->
