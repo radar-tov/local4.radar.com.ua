9a461de0a6eac000dd87d75e9886eea3a69895a1
Evgenii <7985106@mail.ru>
2017-03-13 12:25:59 +0200

    Добавлена валидация отправки почты

--- app/Http/Controllers/Frontend/MailController.php
+++ app/Http/Controllers/Frontend/MailController.php
@@ -38,136 +38,16 @@ class MailController extends Controller
         $this->mail->setFrom($this->emailFrom, 'Radar.com.ua');
 	}
 
-	public function mailMe(Request $request)
-	{
-		$data = $request->all();
-		if($data['_view'] == 'callback'){
-			$messages = [
-				'required' => "Поле :attribute обязательно к заполнению.",
-				'phone' => "Поле :attribute обязательно к заполнению."
-			];
-
-			$this->validate($request, [
-				'name' 	=> 'required|max:255',
-				'phone' => 'required|max:255'
-			], $messages);
-		}
-
-		if($data['_view'] == 'skidka'){
-			$messages = [
-				'required' => "Поле :attribute обязательно к заполнению.",
-				'email' => "Поле :attributeдолжно быть электронным адресом."
-			];
-
-			$this->validate($request, [
-				'name' 	=> 'required|max:255',
-				'phone' => 'required|max:255',
-				'email' => 'required|email',
-				'comment' => 'required|max:255'
-			], $messages);
-		}
-
-        if($data['_view'] == 'oneclick'){
-            $messages = [
-                'required' => "Поле :attribute обязательно к заполнению."
-            ];
-
-            $this->validate($request, [
-                'phone' => 'required|max:255'
-            ], $messages);
-        }
-
-
-
-
-
-
-		//Тема сообщения
-		if($data['_view'] == 'contact'){
-			$data['subject'] = 'Новое оповещение из формы обратной связи.';
-			$message = 'Ваше письмо отправлено!';
-		}elseif($data['_view'] == 'callback'){
-			$data['subject'] = 'Заказ обратного звонка.';
-			$message = '<h3 align="center">Спасибо, мы обязательно с Вами свяжемся.</h3>';
-		}elseif($data['_view'] == 'skidka') {
-            $data['subject'] = 'Запрос на получение скидки.';
-            $message = '<h3 align="center">Ваш запрос расматривается. Мы обязательно с Вами свяжемся.</h3>';
-        }elseif($data['_view'] == 'oneclick') {
-            $data['subject'] = 'Покупка в 1 клик.';
-            $message = '<h3 align="center">Спасибо за покупку. Мы обязательно с Вами свяжемся.</h3>';
-        }else{
-
-		}
-
-		if(empty($data['phone'])){
-			$data['phone'] = 'Не указан.';
-		}
-		if(empty($data['comment'])){
-			$data['comment'] = 'Не указан.';
-		}
-		if(empty($data['email'])){
-			$data['email'] = 'Не указан.';
-		}
-        if(empty($data['name'])){
-            $data['name'] = 'Не указан.';
-        }
-
-		$this->sendMessage($data);
-
-		if($data['_view'] == 'callback'){
-			return response($message);
-			exit;
-		}
-
-		if($data['_view'] == 'skidka'){
-			return response($message);
-			exit;
-		}
-
-        if($data['_view'] == 'oneclick'){
-            return response($message);
-            exit;
-        }
-
-		$request->session()->put('from_otvet', $data['_view']);
-		$request->session()->put('otvet', $message);
-		$request->session()->save();
-
-		return redirect()->back();
-	}
-
-	/**
-	 * @param array $data
-	 * @return bool
-	 */
-	protected function sendMessage(array $data)
-	{
-		$result = Mail::send('mail.contact', ['data' => $data] , function($message) use ($data)
-		{
-			//dd($this->emailFrom, $this->messageTo);
-			$message->from($this->emailFrom, 'Интернет магазин Radar');
-			$message->to($this->emailTo, 'Mr. Admin')->subject($data['subject']);//->subject(array_get($data,'subject',''))
-		});
-
-//		$result = Mail::send('mail.contact', ['data' => $date], function($message) use ($data){
-//
-//			$mail_admin = env('MAIL_ADMIN');
-//
-//			$message->from($data['email'], $data['name']);
-//			$message->to($mail_admin, 'Mr. Admin')->subject('Question');
-//
-//		});
-
-		if($result){
-			return true;
-		}else{
-			return false;
-		}
-
-		return true;
-	}
-
 	public function oneclick(Request $request){
+
+        $messages = [
+            'phone.required' => "Поле ТЕЛЕФОН обязательно к заполнению.",
+            'phone.min' => "Поле ТЕЛЕФОН не может быть менее 10 символов.",
+        ];
+
+        $this->validate($request, [
+            'phone' => 'required|min:10',
+        ], $messages);
 
 	    $data = [
 	        'title' => $request->title,
@@ -190,7 +70,21 @@ class MailController extends Controller
     }
 
     public function contact(Request $request){
-        //dd($request->all());
+
+        $messages = [
+            'name.required' => "Поле ИМЯ обязательно к заполнению.",
+            'name.max' => "Поле ИМЯ не может быть боее 255 символов.",
+            'name.min' => "Поле ИМЯ не может быть менее 3 символов.",
+            'mail.required' => "Поле Email обязательно к заполнению.",
+            'comment.required' => "Поле КОМЕНТАРИИ обязательно к заполнению.",
+        ];
+
+        $this->validate($request, [
+            'name' 	=> 'required|max:255|min:3',
+            'mail' => 'required',
+            'comment' => 'required'
+        ], $messages);
+
 	    $data = [
             'name' => $request->name,
             'email' => $request->email,
@@ -219,6 +113,20 @@ class MailController extends Controller
     }
 
     public function callback(Request $request){
+
+        $messages = [
+            'name.required' => "Поле ИМЯ обязательно к заполнению.",
+            'name.max' => "Поле ИМЯ не может быть боее 255 символов.",
+            'name.min' => "Поле ИМЯ не может быть менее 3 символов.",
+            'phone.required' => "Поле ТЕЛЕФОН обязательно к заполнению.",
+            'phone.min' => "Поле ТЕЛЕФОН не может быть менее 10 символов.",
+        ];
+
+        $this->validate($request, [
+            'name' 	=> 'required|max:255|min:3',
+            'phone' => 'required|min:10'
+        ], $messages);
+
         $data = [
             'name' => $request->name,
             'phone' => $request->phone
@@ -239,6 +147,20 @@ class MailController extends Controller
     }
 
     public function skidka(Request $request){
+
+        $messages = [
+            'name.required' => "Поле ИМЯ обязательно к заполнению.",
+            'name.max' => "Поле ИМЯ не может быть боее 255 символов.",
+            'name.min' => "Поле ИМЯ не может быть менее 3 символов.",
+            'phone.required' => "Поле ТЕЛЕФОН обязательно к заполнению.",
+            'phone.min' => "Поле ТЕЛЕФОН не может быть менее 10 символов."
+        ];
+
+        $this->validate($request, [
+            'name' 	=> 'required|max:255|min:3',
+            'phone' => 'required|min:10'
+        ], $messages);
+
         $data = [
             'name' => $request->name,
             'email' => $request->email,
@@ -259,5 +181,131 @@ class MailController extends Controller
             echo "<h3 align='center'>Ваша заявка принята. В ближайшее время с Вами свяжутся. Спасибо.</h3>";
         }
 
+    }
+
+
+    public function mailMe(Request $request)
+    {
+        $data = $request->all();
+        if($data['_view'] == 'callback'){
+            $messages = [
+                'required' => "Поле :attribute обязательно к заполнению.",
+                'phone' => "Поле :attribute обязательно к заполнению."
+            ];
+
+            $this->validate($request, [
+                'name' 	=> 'required|max:255',
+                'phone' => 'required|max:255'
+            ], $messages);
+        }
+
+        if($data['_view'] == 'skidka'){
+            $messages = [
+                'required' => "Поле :attribute обязательно к заполнению.",
+                'email' => "Поле :attributeдолжно быть электронным адресом."
+            ];
+
+            $this->validate($request, [
+                'name' 	=> 'required|max:255',
+                'phone' => 'required|max:255',
+                'email' => 'required|email',
+                'comment' => 'required|max:255'
+            ], $messages);
+        }
+
+        if($data['_view'] == 'oneclick'){
+            $messages = [
+                'required' => "Поле :attribute обязательно к заполнению."
+            ];
+
+            $this->validate($request, [
+                'phone' => 'required|max:255'
+            ], $messages);
+        }
+
+
+
+
+
+
+        //Тема сообщения
+        if($data['_view'] == 'contact'){
+            $data['subject'] = 'Новое оповещение из формы обратной связи.';
+            $message = 'Ваше письмо отправлено!';
+        }elseif($data['_view'] == 'callback'){
+            $data['subject'] = 'Заказ обратного звонка.';
+            $message = '<h3 align="center">Спасибо, мы обязательно с Вами свяжемся.</h3>';
+        }elseif($data['_view'] == 'skidka') {
+            $data['subject'] = 'Запрос на получение скидки.';
+            $message = '<h3 align="center">Ваш запрос расматривается. Мы обязательно с Вами свяжемся.</h3>';
+        }elseif($data['_view'] == 'oneclick') {
+            $data['subject'] = 'Покупка в 1 клик.';
+            $message = '<h3 align="center">Спасибо за покупку. Мы обязательно с Вами свяжемся.</h3>';
+        }else{
+
+        }
+
+        if(empty($data['phone'])){
+            $data['phone'] = 'Не указан.';
+        }
+        if(empty($data['comment'])){
+            $data['comment'] = 'Не указан.';
+        }
+        if(empty($data['email'])){
+            $data['email'] = 'Не указан.';
+        }
+        if(empty($data['name'])){
+            $data['name'] = 'Не указан.';
+        }
+
+        $this->sendMessage($data);
+
+        if($data['_view'] == 'callback'){
+            return response($message);
+            exit;
+        }
+
+        if($data['_view'] == 'skidka'){
+            return response($message);
+            exit;
+        }
+
+        if($data['_view'] == 'oneclick'){
+            return response($message);
+            exit;
+        }
+
+        $request->session()->put('from_otvet', $data['_view']);
+        $request->session()->put('otvet', $message);
+        $request->session()->save();
+
+        return redirect()->back();
+    }
+
+    protected function sendMessage(array $data)
+    {
+        $result = Mail::send('mail.contact', ['data' => $data] , function($message) use ($data)
+        {
+            //dd($this->emailFrom, $this->messageTo);
+            $message->from($this->emailFrom, 'Интернет магазин Radar');
+            $message->to($this->emailTo, 'Mr. Admin')->subject($data['subject']);//->subject(array_get($data,'subject',''))
+        });
+
+//		$result = Mail::send('mail.contact', ['data' => $date], function($message) use ($data){
+//
+//			$mail_admin = env('MAIL_ADMIN');
+//
+//			$message->from($data['email'], $data['name']);
+//			$message->to($mail_admin, 'Mr. Admin')->subject('Question');
+//
+//		});
+
+        if($result){
+            return true;
+        }else{
+            return false;
+        }
+
+        return true;
     }
 }
