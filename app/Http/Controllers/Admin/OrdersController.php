071608f33dda88946be1b2eff9b44d6b9cf2accf
Evgenii <7985106@mail.ru>
2017-03-15 12:54:47 +0200

    12.54

--- app/Http/Controllers/Admin/OrdersController.php
+++ app/Http/Controllers/Admin/OrdersController.php
@@ -96,10 +96,10 @@ class OrdersController extends AdminController
         Order::find($id)->update($request->all());
 
 	    if((int)$request->get('button')) {
-		    return redirect()->route('dashboard.orders.index');
+		    return redirect()->route('orders.index');
 	    }
 
-	    return redirect()->route('dashboard.orders.show', $id);
+	    return redirect()->route('orders.show', $id);
     }
 
     /**
